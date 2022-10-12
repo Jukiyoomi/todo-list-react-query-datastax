b05cec476d64a6745d98cc04ab2cc18956dc5f7f
Jukiyoomi <jnguessan2003@gmail.com>
2022-10-12 22:26:13 +0200

    added "timestamp" filed to db, created sort by timestamp function

--- controllers/todo.controllers.js
+++ controllers/todo.controllers.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 module.exports.getTodos = async (req, res) => {
     try {
-        const { data, status } = await axios.get(
+        let { data, status } = await axios.get(
             'https://cefb09fe-9970-4f0c-bfb5-8c8d865bf50c-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/todos/collections/list?page-size=20',
             {
                 headers: {
@@ -16,15 +16,20 @@ module.exports.getTodos = async (req, res) => {
             }
         )
         console.log(status)
+        // data.sort(([a, b]) => a.createdAt < b.createdAt)
+        data = {data:sortObject(data.data, "createdAt")}
+        // console.log(sortObject(data, "createdAt"))
         res.json(data)
     } catch (error) {
         res.json({ message: 'Error', data: error })
     }
 }
 
+
+
 module.exports.getCompletedTodos = async (req, res) => {
     try {
-        const { data, status } = await axios.get(
+        let { data, status } = await axios.get(
             'https://cefb09fe-9970-4f0c-bfb5-8c8d865bf50c-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/todos/collections/list?where=%7B%22completed%22%3A%7B%22%24eq%22%3Atrue%7D%7D&page-size=20',
             {
                 headers: {
@@ -34,6 +39,7 @@ module.exports.getCompletedTodos = async (req, res) => {
             }
         )
         console.log(status)
+        data = {data:sortObject(data.data, "createdAt")}
         res.json(data)
     } catch (error) {
         res.json({ message: 'Error', data: error })
@@ -42,7 +48,7 @@ module.exports.getCompletedTodos = async (req, res) => {
 
 module.exports.getNonCompletedTodos = async (req, res) => {
     try {
-        const { data, status } = await axios.get(
+        let { data, status } = await axios.get(
             'https://cefb09fe-9970-4f0c-bfb5-8c8d865bf50c-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/todos/collections/list?where=%7B%22completed%22%3A%7B%22%24eq%22%3Afalse%7D%7D&page-size=20',
             {
                 headers: {
@@ -52,6 +58,7 @@ module.exports.getNonCompletedTodos = async (req, res) => {
             }
         )
         console.log(status)
+        data = {data:sortObject(data.data, "createdAt")}
         res.json(data)
     } catch (error) {
         res.json({ message: 'Error', data: error })
@@ -66,6 +73,7 @@ module.exports.createTodo = async (req, res) => {
             'https://cefb09fe-9970-4f0c-bfb5-8c8d865bf50c-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/todos/collections/list',
             {
                 ...req.body,
+                createdAt: Date.now(),
                 completed: false,
             },
             {
@@ -105,7 +113,7 @@ module.exports.deleteTodo = async (req, res) => {
     try {
         const { data, status } = await axios.delete(
             'https://cefb09fe-9970-4f0c-bfb5-8c8d865bf50c-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/todos/collections/list/' +
-                req.params.id,
+            req.params.id,
             {
                 headers: { accept: 'application/json', 'X-Cassandra-Token': process.env.CASSANDRA_TOKEN },
             }
@@ -116,3 +124,38 @@ module.exports.deleteTodo = async (req, res) => {
         res.json({ message: 'Error', data: error })
     }
 }
+
+function sortObject(data, attr) {
+    let arr = [];
+    for (let prop in data) {
+        if (data.hasOwnProperty(prop)) {
+            let obj = {};
+            obj[prop] = data[prop];
+            obj.tempSortName = data[prop][attr];
+            arr.push(obj);
+        }
+    }
+
+    arr.sort(function(a, b) {
+        let at = a.tempSortName,
+            bt = b.tempSortName;
+        return at < bt ? 1 : ( at > bt ? -1 : 0 );
+    });
+
+    let result = [];
+
+    for (let i=0, l=arr.length; i<l; i++) {
+        let obj = arr[i];
+        delete obj.tempSortName;
+        let id
+        for (let prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                id = prop;
+            }
+        }
+        let item = obj[id];
+        result.push(item);
+    }
+    return result;
+}
+
