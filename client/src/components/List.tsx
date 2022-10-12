ec01ef8e243431a8bbbdf0cda21d63908b8c3d2a
Jukiyoomi <jnguessan2003@gmail.com>
2022-10-12 20:38:25 +0200

    used React-Query to get todos and refresh request when option changes

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,41 +1,33 @@
-import React from 'react';
-import data from '../data.json'
+import React, {useEffect} from 'react';
 import Todo from "./Todo";
+import {useQuery} from "react-query";
+import axios from "axios";
 
+const List = ({url}: {url: string}) => {
 
-const List = () => {
-	console.log(data.data)
+	const {data, refetch, isFetching, isLoading} = useQuery(['todos'], () => {
+		return axios.get(url).then((res) => res.data)
+	})
+	useEffect(() => {
+		refetch()
+		console.log(url)
+	}, [url])
+	// console.log(data)
+
 	return (
 		<ul className="flex-1 flex flex-col gap-4 min-w-max">
-			{/*{Object.entries(data.data).map(([key, value])=> <p key={key}>{JSON.stringify(value)}</p>)}*/}
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
-			<Todo />
+			<>
+				{isLoading || isFetching ? <p>Loading...</p> :
+					(Object.entries(data.data).length > 0 ?
+						<>
+							<p>Total : {Object.entries(data.data).length}</p>
+							{Object.entries(data.data).map(([key, value])=> <Todo key={key} id={key} todo={value}/>)}
+						</> :
+						<p>No todos</p>
+					)
+				}
+			</>
+
 		</ul>
 
 	);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+
+const client = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60_000
+        }
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={client}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
     </React.StrictMode>
 )
