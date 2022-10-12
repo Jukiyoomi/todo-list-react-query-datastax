129e298f97eaf15b00ef239f8da78615d2c1c830
Jukiyoomi <jnguessan2003@gmail.com>
2022-10-12 22:20:54 +0200

    post new todo created, and form reset

--- client/src/components/Todo.tsx
+++ client/src/components/Todo.tsx
@@ -7,13 +7,15 @@ type PropType = {id: string, todo: Todo}
 const Todo = ({id, todo}: PropType) => {
 	return (
 		<li
-			className="todo relative flex justify-between md:flex-row gap-4 items-center px-6 bg-white py-3 rounded-xl shadow shadow-xl hover:shadow-lg transition border-l-8 border-l-green-700"
+			className={`todo relative flex justify-between md:flex-row gap-4 items-center px-6 bg-white py-3 rounded-xl shadow shadow-xl hover:shadow-lg transition border-l-8 ${todo.completed ? 'border-l-green-700' : 'border-l-red-700'}`}
 			style={{minWidth: 320}}
 		>
 			<p className="font-bold text-xl">{todo.content}</p>
-			{/*<button className="cursor-pointer text-sm">Complete This todo</button>*/}
-			<AiOutlineCheckCircle className="cursor-pointer text-xl text-xl text-green-700" />
-			<AiOutlineCloseCircle className="cursor-pointer text-xl text-xl text-red-700" />
+			{
+				todo.completed ?
+					<AiOutlineCloseCircle className="cursor-pointer text-xl text-xl text-red-700" />
+					: <AiOutlineCheckCircle className="cursor-pointer text-xl text-xl text-green-700" />
+			}
 		</li>
 
 	);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {QueryClient, QueryClientProvider} from "react-query";
+import {QueryClient, QueryClientProvider, useQueryClient} from "react-query";
 import {ReactQueryDevtools} from "react-query/devtools";
 
 const client = new QueryClient({
     defaultOptions: {
         queries: {
             staleTime: 60_000
-        }
+        },
+
     }
 })
 
